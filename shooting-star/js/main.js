c1a47e491598580bbb30ebb2b77667e7b10d4452
Michael Habron <habronm@gmail.com>
2019-02-06 20:14:16 -0500

    updated first game

---
+++ shooting-star/js/main.js
@@ -0,0 +1,225 @@
+"use strict";
+
+window.onload = function() {
+    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
+    // You will need to change the fourth parameter to "new Phaser.Game()" from
+    // 'phaser-example' to 'game', which is the id of the HTML element where we
+    // want the game to go.
+    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
+    // You will need to change the paths you pass to "game.load.image()" or any other
+    // loading functions to reflect where you are putting the assets.
+    // All loading functions will typically all be found inside "preload()".
+    
+    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+    
+    function preload() {
+        // Load an image and call it 'logo'.
+        game.load.spritesheet( 'cat', 'assets/cat-enemy.png', 64, 64);
+		game.load.image( 'player', 'assets/treat-bag-player.png');
+		game.load.spritesheet( 'bullet', 'assets/snack-bullet.png', 64, 64);
+		game.load.spritesheet( 'enemyBullet', 'assets/paw-enemy-bullet-small.png', 64, 64);
+		game.load.image('background', 'assets/background.png');
+		
+    }
+    
+    var playerBag;
+	var catEnemies;
+	var bulletSnacks;
+	var bulletTime = 0;
+	var keys;
+	var fireButton;
+	var explosions;
+	var score = 0;
+	var scoreString = '';
+	var scoreText;
+	var lives;
+	var enemyBullet;
+	var background;
+	var firingTimer = 0;
+	var spawnTimer = 0;
+	var livingEnemies = [];
+	
+    
+    function create() {
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+		
+		background = game.add.tileSprite(0, 0, 800, 600, 'background');
+		
+		bulletSnacks = game.add.group();
+		bulletSnacks.enableBody = true;
+		bulletSnacks.physicsBodyType = Phaser.Physics.ARCADE;
+		bulletSnacks.createMultiple(20, 'bullet', [0,1]);
+		bulletSnacks.setAll('anchor.x', 0.5);
+		bulletSnacks.setAll('anchor.y', 1);
+		bulletSnacks.setAll('outOfBoundsKill', true);
+		bulletSnacks.setAll('checkWorldBounds', true);
+		
+		bulletPaws = game.add.group();
+		bulletPaws.enableBody = true;
+		bulletPaws.physicsBodyType = Phaser.Physics.ARCADE;
+		bulletPaws.createMultiple(20, 'enemyBullet', [0,1,2,3]);
+		bulletPaws.setAll('anchor.x', 0.5);
+		bulletPaws.setAll('anchor.y', 1);
+		bulletPaws.setAll('outOfBoundsKill', true);
+		bulletPaws.setAll('checkWorldBounds', true);
+		
+		playerBag = game.add.sprite(400, 500, 'player');
+		playerBag.anchor.setTo(0.5, 0.5);
+		game.physics.enable(playerBag, Phaser.Physics.ARCADE);
+		
+		catEnemies = game.add.group();
+		catEnemies.enableBody = true;
+		catEnemies.physicsBodyType = Phaser.Physics.ARCADE;
+		catEnemies.setAll('outofBoundsKill', true);
+		catEnemies.setAll('checkWorldBounds', true);
+		
+		createCat();
+		
+		scoreString = 'Score: ';
+		scoreText = game.add.text(10,10, scoreString + score, {font: '34px Impact', fill '#fff'});
+		
+		lives = game.add.group();
+		game.add.text(game.world.width - 100, 10, 'Lives: ', {font: '34px Impact', fill '#fff'});
+		
+		for (var i = 0; i < 3; i++) {
+			var bag = lives.create(game.world.width - 100 + (30 * i), 60, 'player');
+			bag.anchor.setTo(0.5, 0.5);
+			bag.angle = 90;
+			bag.alpha = 0.4;
+		}
+    //  An explosion pool
+    //explosions = game.add.group();
+    //explosions.createMultiple(30, 'kaboom');
+    //explosions.forEach(setupInvader, this);
+
+		//  And some controls to play the game with
+		keys = game.input.keyboard.createCursorKeys();
+		fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		
+    }
+    function createCat() {
+		var cat = catEnemies.create(game.rnd.integerinRange(200, 600), 0, 'cat');
+		cat.animations.add('move', [0,1,2,3], 5, true);
+		cat.play('move');
+		while(cat.y < 850) {
+			cat.y += 4;
+		}
+		spawnTimer = game.time.now + 2000;
+		livingEnemies.push[cat];
+	}
+    function update() {
+        background.tilePosition.y += 2;
+		if (playerBag.alive) {
+			playerBag.body.velocity.setTo(0, 0);
+			if (cursors.left.isDown) {
+				player.body.velocity.x = -200;
+			}
+			else if (cursors.right.isDown) {
+				player.body.velocity.x = 200;
+			}
+
+			//  Firing?
+			if (fireButton.isDown) {
+				fireBullet();
+			}
+
+			if (game.time.now > firingTimer) {
+				enemyFires();
+			}
+			if (game.time.now > spawnTimer) {
+				createCat();
+			}
+			//  Run collision
+			game.physics.arcade.overlap(bulletSnacks, catEnemies, collisionHandler, null, this);
+			game.physics.arcade.overlap(bulletPaws, playerBag, enemyHitsPlayer, null, this);
+		}
+    }
+	function collisionHandler (bullet, cat) {
+
+		//  When a bullet hits an alien we kill them both
+		bullet.kill();
+		cat.kill();
+
+		//  Increase the score
+		score += 50;
+		scoreText.text = scoreString + score;
+
+		//  And create an explosion :)
+		//var explosion = explosions.getFirstExists(false);
+		//explosion.reset(cat.body.x, cat.body.y);
+		//explosion.play('kaboom', 30, false, true);
+	}
+
+function enemyHitsPlayer (player,bullet) {
+    
+    bullet.kill();
+
+    live = lives.getFirstAlive();
+
+    if (live)
+    {
+        live.kill();
+    }
+
+    //  And create an explosion :)
+    //var explosion = explosions.getFirstExists(false);
+    //explosion.reset(playerBag.body.x, playerBag.body.y);
+    //explosion.play('kaboom', 30, false, true);
+
+    // When the player dies
+    if (lives.countLiving() < 1) {
+        player.kill();
+        bulletPaws.callAll('kill');
+
+        stateText.text=" GAME OVER \n Click to restart";
+        stateText.visible = true;
+
+        //the "click to restart" handler
+        game.input.onTap.addOnce(restart,this);
+    }
+
+}
+
+	function enemyFires () {
+		//  Grab the first bullet we can from the pool
+		enemyBullet = bulletPaws.getFirstExists(false);
+		if (enemyBullet && livingEnemies.length > 0) {
+        
+			var random=game.rnd.integerInRange(0,livingEnemies.length-1);
+
+			// randomly select one of them
+			var shooter=livingEnemies[random];
+			// And fire the bullet from this enemy
+			enemyBullet.reset(shooter.body.x, shooter.body.y);
+
+			game.physics.arcade.moveToObject(enemyBullet,playerBag,120);
+			firingTimer = game.time.now + 1500;
+		}
+	}
+
+	function fireBullet () {
+
+		//  To avoid them being allowed to fire too fast we set a time limit
+		if (game.time.now > bulletTime)
+		{
+			//  Grab the first bullet we can from the pool
+			bullet = bulletSnacks.getFirstExists(false);
+
+			if (bullet)
+			{
+				//  And fire it
+				bullet.reset(playerBag.x, playerBag.y + 8);
+				bullet.body.velocity.y = -400;
+				bulletTime = game.time.now + 200;
+			}
+		}
+
+	}
+	function resetBullet(bullet) {
+		bullet.kill();
+	}
+	function resetCat(cat) {
+		livingEnemies.pop(cat);
+		cat.kill();
+	}
+};
